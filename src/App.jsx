75061b7dd0b92e3c903b4bbeb521e838b2f52a19
Mathias Talo <mathias.talo@outlook.com>
2024-05-27 15:51:30 +0300

    Testing success page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./Homepage"
 import ContactPage from "./ContactPage.jsx";
 import LinksPage from "./LinksPage.jsx";
 import Projects from "./Projects.jsx";
+import Success from "./Success.jsx";
 import OpenTrackFestival2024 from "./projects/open-track-festival-2024/OpenTrackFestival2024.jsx";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/links" element={<LinksPage />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/contact/success" element={<Success />} />
 
           <Route path="/projects/opentrackfestival2024" element={<OpenTrackFestival2024 />} />
         </Routes>
---
+++ src/Success.jsx
@@ -0,0 +1,9 @@
+function Success() {
+    return (
+      <div className="">
+        <h2>NICE</h2>
+      </div>
+    )
+  }
+  
+  export default Success;
